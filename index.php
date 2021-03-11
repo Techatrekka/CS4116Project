2c859521ad264c993f01098fa81202d381591c32
Sean <11585008+Techatrekka@users.noreply.github.com>
2021-03-11 17:10:43 +0000

    Added Landing Page completed

--- index.php
+++ index.php
@@ -1,25 +1,105 @@
-<html>
-<head>
-<title>EmployMee</title>
-</head>
-<body>
-<?php
-$servername = "sql201.epizy.com";
-$username = "epiz_28051136";
-$password = "33mkLIcjQzUnDp";
-$dbname = "epiz_28051136_test";
-$conn = new mysqli($servername, $username, $password,$dbname);// Check connection
-if ($conn->connect_error) {    
-die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-$sql = "select * from modules;";
-$result = $conn->query($sql); 
-while($row = $result->fetch_assoc())   {    
-print "{$row["code"]}:{$row["name"]}\n";  
-}
-$conn->close();
-?>
-<p>This is a website!</p>
-</body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>EmployMee</title>
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+	<div class="row align-items-start">
+    <div class="col">
+	<img src="mockups\leftColimg.png" height="350" width="350" style="padding-top: 100px; padding-left:30px;">
+    </div>
+    <div class="col">
+		<nav class="navbar navbar-expand-sm navbar-light justify-content-end">
+		  <div class="container-fluid">
+				<img src="mockups/EmployMeeLogo2.png" class="img-thumbnail" alt="Logo" width="150" height="150">
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			  <span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			  <div class="navbar-nav">
+				<a class="nav-link active" aria-current="page" href="#">Home</a>
+				<a class="nav-link" href="#">About</a>
+				<a class="nav-link" href="#">Vacancies</a>
+			  </div>
+			  <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
+					<ul class="navbar-nav text-right">
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Login/Signup</a>
+						</li>
+					</div>
+			  </div>
+			</div>
+		</nav>
+		<img src="mockups\centreColImage.png" width="350" height="350">		
+	</div>
+    <div class="col">
+		<div class="col" style="color:MidnightBlue;padding-top: 150px;padding-right:30px;">
+				<h3><b>What is EmployMee?</b></h3>
+				<p><b>EmployMee is a social networking site that allows users to craft 
+				a descriptive professional profile. The site allows you to search for 
+				jobs based on your own skills while simultaneously offering companies
+				a platform to advertise vacancies and search for potential employees 
+				based on their skills and past work experience.</b></p>
+			</div>
+    </div>
+  </div>
+  <div class="container-fluid" style="background-color:MidnightBlue;">
+   <div class="row align-items-start" style="padding-top:50px;padding-bottom:50px;">
+			<div class="col">
+			</div>
+			<div class="col">
+			<div class="row align-items-center" style="color:White;">
+				<div class="col">
+					<img src="mockups\midcolleft.png">
+					<h5>Grow Your Network</h5>
+					<p>Connect with other users around the world
+					and have the network you always wanted.</p>
+				</div>
+				<div class="col">
+					<img src="mockups\midcolcentre.png">
+					<h5>Find New Jobs</h5>
+					<p>Find new job oppurtunities by searching
+					through vacancies and apply for your dream job.</p>
+				</div>
+				<div class="col">
+					<img src="mockups\midcolright.png">
+					<h5>Portfolio</h5>
+					<p>Create a portfolio for prospective employers to view</p>
+				</div>
+		</div>
+		</div>
+			<div class="col">
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row align-items-center">
+			<div class="col">
+			</div>
+			<div class="col">
+				<div class="row align-items-center" style="a:Gold;">
+					<div class="col">
+						<img src="mockups/EmployMeeLogo2.png" class="img-thumbnail" alt="Logo" width="150" height="150">
+					</div>
+					<div class="col">
+						<a class="nav-link" href="#">Information</a>
+					</div>
+					<div class="col">
+						<a href="#">Resources</a>
+					</div>
+					<div class="col">
+						<a href="#">Contact Us</a>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+			</div>
+		</div>
+	</div>
+  </body>
 </html>
